c6d06ce5de1746637982f7cccafd83c085cac4e5
Josué Elias <=>
2018-02-23 16:57:58 -0300

    finished

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-console.log("Hello from app.js");
+import css from './stylesheet.css'
+import Hello from './hello'
+
+(new Hello({
+    target: document.getElementsByTagName('main')[0]
+})).run()
+
+console.log("Hello from app.js")
+
